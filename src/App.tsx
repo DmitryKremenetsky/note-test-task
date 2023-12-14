63fe7d21657a7a1fc91ceeeb42234a9af574a47f
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-12-14 12:45:04 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, CssBaseline } from "@material-ui/core";
+import NoteList from "./components/NoteList";
+import NoteEditor from "./components/NoteEditor";
+import TagFilter from "./components/TagFilter";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="md">
+      <CssBaseline />
+      <TagFilter />
+      <NoteEditor />
+      <NoteList />
+    </Container>
   );
-}
+};
 
 export default App;
